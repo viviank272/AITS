1cd7f3d9c48fd424bee5f101ecdf1ea393a2dd43
viviank272 <viviankomuhendo@gmail.com>
2025-03-31 17:19:12 +0300

    moved for organization

---
+++ src/components/dashboard/StudentSidebar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { ListGroup } from "react-bootstrap";
+
+const StudentSidebar = () => {
+  return (
+    <div className="bg-primary text-black vh-100 p-3">
+      <h4>Student Panel</h4>
+      <ListGroup variant="flush">
+        <ListGroup.Item className="bg-primary border-0">
+          <Link to="/student-dashboard" className="text-white">Dashboard</Link>
+        </ListGroup.Item>
+        <ListGroup.Item className="bg-primary border-0">
+          <Link to="/courses" className="text-white">My Courses</Link>
+        </ListGroup.Item>
+        <ListGroup.Item className="bg-primary border-0">
+          <Link to="/assignments" className="text-white">Assignments</Link>
+        </ListGroup.Item>
+        <ListGroup.Item className="bg-primary border-0">
+          <Link to="/reports" className="text-white">Performance Reports</Link>
+        </ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
+};
+
+export default StudentSidebar;
+
