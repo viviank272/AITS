ffc273d73c8cddb3482048c74dacd4e0aeba40ec
Benita-N655 <nalungabenita1@gmail.com>
2025-04-02 12:24:02 +0300

    models added

---
+++ models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User  
+import json
+
+class College(models.Model):
+    college_id = models.AutoField(primary_key=True)
+    college_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.college_name
+
+
+class Department(models.Model):
+    department_id = models.AutoField(primary_key=True)
+    department_name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    head_user_id = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True) 
+    college_id = models.ForeignKey(College, on_delete=models.CASCADE) 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.department_name
+
+
+class Role(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
+    permissions = models.JSONField() 
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.role_name
+
+
