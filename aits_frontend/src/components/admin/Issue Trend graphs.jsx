6d4221dc99e53f6f1c0061efd396422319c04691
viviank272 <viviankomuhendo@gmail.com>
2025-04-11 16:32:55 +0300

    Update issue trend graph component

--- aits_frontend/src/components/admin/Issue Trend graphs.jsx
+++ aits_frontend/src/components/admin/Issue Trend graphs.jsx
@@ -1,6 +1,6 @@
 import React from  "react";
 import { Line } from "react-chartjs-2";
-import { Chart as ChartJS, LineElement, CategoryScale, LinearScale, PointElement, Title, Tooltip, Legend } from "chart.js";
+import { Chart as ChartJS, LineElement, CategoryScale, PointElement, Title, Tooltip, Legend } from "chart.js";
 
 
 
