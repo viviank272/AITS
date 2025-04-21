a1ba2221ec02d9956667428bd4f96b46680f07b5
Benita-N655 <nalungabenita1@gmail.com>
2025-04-21 08:04:45 +0300

    Update views

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from .models import Department, Role
+from .models import Department, Role, User
 from .serializers import DepartmentSerializer, RoleSerializer
 from rest_framework.decorators import action
 
