7e49d139d3cc05e67247f1c49588296de107ad83
Benita-N655 <nalungabenita1@gmail.com>
2025-04-21 08:15:22 +0300

    Update urls

--- urls.py
+++ urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import DepartmentViewSet, RoleViewSet
+from .views import DepartmentViewSet, RoleViewSet, UserViewSet
 
 router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet)
@@ -8,4 +8,11 @@ router.register(r'roles', RoleViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)), 
+]
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
 ]
